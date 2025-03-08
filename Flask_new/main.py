595e70afee30f6c30c896b24408986b98e9aaeef
Тамирон Пост <150392282+Tami555@users.noreply.github.com>
2025-03-09 02:15:28 +0300

    Cabin_color1.py

--- Flask_new/main.py
+++ Flask_new/main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, redirect
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, SelectField,  IntegerField 
 from wtforms.validators import DataRequired
 
 app= Flask(__name__)
@@ -16,7 +16,14 @@ class LoginForm(FlaskForm):
 
     submit = SubmitField('Войти')
 
-@app.route('/')
+
+class Person(FlaskForm):
+    name = StringField('Введите имя', validators=[DataRequired()])
+    age =  IntegerField ('Введите возраст', validators=[DataRequired()])
+    sex = SelectField('Ваш пол:', choices=[('man', 'Мужской'), ('woman', 'Женский')], validators=[DataRequired()])
+    submit = SubmitField('Получить каюту')
+
+@app.route('/base')
 def base():
     return render_template('base.html')
     
@@ -24,9 +31,7 @@ def base():
 def index():
     form = LoginForm()
     if form.validate_on_submit():
-        print('все норм')
         return redirect('/success')
-    print('все плохо')
     return render_template('index.html', title='Авторизация', form=form)
 
 @app.route('/success')
@@ -37,6 +42,21 @@ def success():
 def distribution():
     workers = ['Ридли Скотт', 'Энди Уир', 'Марк Уонти', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
     return render_template('distribution.html', workers=workers)
+
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Person()
+    if form.validate_on_submit():
+        return redirect(f'/table/{form.sex.data}/{form.age.data}')
+    
+    return render_template('login.html', form=form)
+    
+@app.route('/table/<sex>/<age>')
+def table(sex, age):
+    return render_template('table.html', sex=sex, age=int(age))
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
