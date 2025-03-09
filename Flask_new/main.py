ca135a99cb4ed6a533f28a257705e67ff032b217
Тамирон Пост <150392282+Tami555@users.noreply.github.com>
2025-03-09 16:32:39 +0300

    Gallery with download

--- Flask_new/main.py
+++ Flask_new/main.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template, url_for, redirect
+from flask import Flask, render_template, url_for, redirect, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField,  IntegerField 
+from wtforms import StringField, PasswordField, SubmitField, SelectField,  IntegerField, FileField
 from wtforms.validators import DataRequired
+import os
 
 app= Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-
+app.config['UPLOAD_FOLDER'] = os.path.join('static', 'img', 'img_carousel')
 
 class LoginForm(FlaskForm):
     astro_name = StringField('Id астронавта', validators=[DataRequired()])
@@ -22,6 +23,10 @@ class Person(FlaskForm):
     age =  IntegerField ('Введите возраст', validators=[DataRequired()])
     sex = SelectField('Ваш пол:', choices=[('man', 'Мужской'), ('woman', 'Женский')], validators=[DataRequired()])
     submit = SubmitField('Получить каюту')
+
+class Load_Images(FlaskForm):
+    file = FileField('Добавьте картинку', validators=[DataRequired()])
+    submit = SubmitField('Загрузить')
 
 @app.route('/base')
 def base():
@@ -43,7 +48,7 @@ def distribution():
     workers = ['Ридли Скотт', 'Энди Уир', 'Марк Уонти', 'Венката Капур', 'Тедди Сандерс', 'Шон Бин']
     return render_template('distribution.html', workers=workers)
 
-@app.route('/', methods=['GET', 'POST'])
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = Person()
@@ -57,6 +62,21 @@ def table(sex, age):
     return render_template('table.html', sex=sex, age=int(age))
 
 
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/galery', methods=['GET', 'POST'])
+def carousel():
+    load = Load_Images()
+
+    if request.method == 'POST':
+        if load.validate_on_submit():
+            f = load.file.data 
+            filename = f.filename
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename) # Формируем путь для сохранения
+            f.save(file_path)
+
+    lst_img = os.listdir(app.config['UPLOAD_FOLDER'])
+    lst_img = [f'img/img_carousel/{x}' for x in lst_img]
+    return render_template('carousel.html', lst_image=lst_img, form=load)
 
 if __name__ == '__main__':
     app.run(debug=True)
